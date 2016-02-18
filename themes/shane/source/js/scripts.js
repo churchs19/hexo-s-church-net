6395458f872d46c56cd5d6347864f41a3e68d766
Shane Church <shane@s-church.net>
2016-02-17 23:27:47 -0600

    Completed article page formatting and functionality and contact me section

--- themes/shane/source/js/scripts.js
+++ themes/shane/source/js/scripts.js
@@ -89,43 +89,65 @@ jQuery(function ($) {
         });
     }());
 
+    // -------------------------------------------------------------
+    // SHARE LINKS
+    // -------------------------------------------------------------
+    $(document).ready(function () {
+        $('body').on('click', function () {
+            $('.article-share-box.on').removeClass('on');
+        }).on('click', '.article-share-link', function (e) {
+            e.stopPropagation();
 
-    // -------------------------------------------------------------
-    // Progress Bar
-    // -------------------------------------------------------------
+            var $this = $(this),
+                url = $this.attr('data-url'),
+                encodedUrl = encodeURIComponent(url),
+                id = 'article-share-box-' + $this.attr('data-id'),
+                offset = $this.offset(),
+                box;
 
-    $('.skill-progress').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            $.each($('div.progress-bar'), function () {
-                $(this).css('width', $(this).attr('aria-valuenow') + '%');
-            });
-            $(this).unbind('inview');
-        }
-    });
+            if ($('#' + id).length) {
+                box = $('#' + id);
 
-    // -------------------------------------------------------------
-    // More skill
-    // -------------------------------------------------------------
-    $('.more-skill').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            $('.chart').easyPieChart({
-                //your configuration goes here
-                easing: 'easeOut',
-                delay: 3000,
-                barColor: '#68c3a3',
-                trackColor: 'rgba(255,255,255,0.2)',
-                scaleColor: false,
-                lineWidth: 8,
-                size: 140,
-                animate: 2000,
-                onStep: function (from, to, percent) {
-                    this.el.children[0].innerHTML = Math.round(percent);
+                if (box.hasClass('on')) {
+                    box.removeClass('on');
+                    return;
                 }
+            } else {
+                var html = [
+                    '<div id="' + id + '" class="article-share-box">',
+                    '<input class="article-share-input" value="' + url + '">',
+                    '<div class="article-share-links">',
+                    '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+                    '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+                    '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+                    '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+                    '</div>',
+                    '</div>'
+                ].join('');
 
-            });
-            $(this).unbind('inview');
-        }
+                box = $(html);
+
+                $('body').append(box);
+            }
+
+            $('.article-share-box.on').hide();
+
+            box.css({
+                top: offset.top + 25,
+                left: offset.left + (box.width() / 2) - ($this.width() / 2)
+            }).addClass('on');
+        }).on('click', '.article-share-box', function (e) {
+            e.stopPropagation();
+        }).on('click', '.article-share-box-input', function () {
+            $(this).select();
+        }).on('click', '.article-share-box-link', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
+        });
     });
+
 
     // -------------------------------------------------------------
     // STELLAR FOR BACKGROUND SCROLLING
@@ -162,9 +184,9 @@ jQuery(function ($) {
     // https://api.instagram.com/v1/users/1511680150/media/recent?client_id=b6f5ef5726a74224b8dbc213f1f64432
     $(document).ready(function () {
         if ($('.photos-section').length > 0) {
-            var $grid = $('.photos-section ul.grid');
-            var imageTemplate = _.template('<li><figure><img src="<%- images.low_resolution.url %>" alt=""><figcaption><div class="caption-content"><a href="<%- images.standard_resolution.url %>" class="single_image" data-title="<%- caption.text %> - <%- created_time_formatted %>" data-link="<%- link %>" data-fancybox-group="gallery"><i class="fa fa-picture-o"></i><p><%- caption.text %></p><p><%- created_time_formatted %></p></a></div></figcaption></figure></li>');
-            var videoTemplate = _.template('<li><figure><img src="<%- images.low_resolution.url %>" alt=""><figcaption><div class="caption-content"><a href="<%- videos.standard_resolution.url %>" class="single_image fancybox.html" data-title="<%- caption.text %> - <%- created_time_formatted %>" data-link="<%- link %>" data-poster="<%- images.standard_resolution.url %>" data-width="<%- videos.standard_resolution.width %>" data-height="<%- videos.standard_resolution.height %>" data-fancybox-group="gallery"><i class="fa fa-video-camera"></i><p><%- caption.text %></p><p><%- created_time_formatted %></p></a></div></figcaption></figure></li>');
+            var $grid = $('.photos-section ul.grid'),
+                imageTemplate = _.template('<li><figure><img src="<%- images.low_resolution.url %>" alt=""><figcaption><div class="caption-content"><a href="<%- images.standard_resolution.url %>" class="single_image" data-title="<%- caption.text %> - <%- created_time_formatted %>" data-link="<%- link %>" data-fancybox-group="gallery"><i class="fa fa-picture-o"></i><p><%- caption.text %></p><p><%- created_time_formatted %></p></a></div></figcaption></figure></li>'),
+                videoTemplate = _.template('<li><figure><img src="<%- images.low_resolution.url %>" alt=""><figcaption><div class="caption-content"><a href="<%- videos.standard_resolution.url %>" class="single_image fancybox.html" data-title="<%- caption.text %> - <%- created_time_formatted %>" data-link="<%- link %>" data-poster="<%- images.standard_resolution.url %>" data-width="<%- videos.standard_resolution.width %>" data-height="<%- videos.standard_resolution.height %>" data-fancybox-group="gallery"><i class="fa fa-video-camera"></i><p><%- caption.text %></p><p><%- created_time_formatted %></p></a></div></figcaption></figure></li>');
             $.ajax({
                 url: 'https://api.instagram.com/v1/users/1511680150/media/recent?client_id=b6f5ef5726a74224b8dbc213f1f64432',
                 crossDomain: true,
@@ -187,7 +209,7 @@ jQuery(function ($) {
                                 this.title = '<a href="' + $(this.element).attr('data-link') + '" target="_blank">' + $(this.element).attr('data-title') + '</a>';
                             },
                             beforeLoad: function () {
-                                if($(this.element).hasClass('fancybox.html')) {
+                                if ($(this.element).hasClass('fancybox.html')) {
                                     // build the HTML5 video structure for fancyBox content with specific parameters
                                     // set fancyBox content and pass parameters
                                     this.content = '<video src="' + this.href + '"  poster="' + $(this.element).attr('data-poster') + '" width="' + $(this.element).attr('data-width') + '" height="' + $(this.element).attr('data-height') + '"  controls="controls" preload="none" ></video>';
@@ -196,20 +218,20 @@ jQuery(function ($) {
                                     this.height = $(this.element).attr('data-height');
                                 }
                             }
-                        
+
                         });
                 }
             });
         }
     });
-    
+
     // -------------------------------------------------------------
     // Scalable Images
     // -------------------------------------------------------------
     $(document).ready(function () {
-        $('img.scale').imageScale();        
+        $('img.scale').imageScale();
     });
-    
+
     // -------------------------------------------------------------
     // Image Gallery
     // -------------------------------------------------------------
@@ -217,10 +239,8 @@ jQuery(function ($) {
         $('a.image-link')
             .fancybox({
                 padding: 4,
-                beforeShow: function () {
-                },
-                beforeLoad: function () {
-                }
+                beforeShow: function () {},
+                beforeLoad: function () {}
             });
 
     });
